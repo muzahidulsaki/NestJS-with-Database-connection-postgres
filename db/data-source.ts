191384e584414f9fc4ca37709f677c610981a6c5
Muzahidul Islam Saki <muzahidulislamsaki@gmail.com>
2025-10-27 08:19:04 +0600

    first commit

---
+++ db/data-source.ts
@@ -0,0 +1,19 @@
+import { DataSource,DataSourceOptions } from 'typeorm';
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [],
+  migrations: [],
+  logging: false,
+  synchronize: true,
+};
+
+const dataSource = new DataSource(dataSourceOptions);
+export default dataSource;
